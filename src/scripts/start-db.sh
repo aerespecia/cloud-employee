d6474e70e288050d85103dbd39810c577b558f46
arespecia <arespecia@placester.com>
2020-11-16 12:31:23 +0800

    minor tweaks on bash scripts

--- src/scripts/start-db.sh
+++ src/scripts/start-db.sh
@@ -3,18 +3,16 @@ set -e
 
 SERVER="cloud_test_server";
 PW="cloudTest123!";
-DB="cloud_test";
+DB="cloud_db";
 
 echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
-(docker kill $SERVER || :) && \
-  (docker rm $SERVER || :) && \
-  docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
+docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
   -e PGPASSWORD=$PW \
   -d postgres
 
 # wait for pg to start
 echo "sleep wait for pg-server [$SERVER] to start";
-sleep 3;
+sleep 5;
 
 # create the db 
 echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
